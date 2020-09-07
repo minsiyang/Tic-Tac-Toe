0160f7f0ae75056c935a776a1b2cea619b02d322
Minsi Yang <yangminsi@gmail.com>
2020-09-07 15:53:34 +0100

    Add Hot Module Replacement, start script for package.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import "./App.css"
+import { hot } from "react-hot-loader";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello, World!</h1>
+        <h3>Will this work now?</h3>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
